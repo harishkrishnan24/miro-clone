500768c236a697fa50fb3769ea2ef068874de01e
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-07-11 21:18:57 +0530

    basic layout

---
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function OrgSidebar() {
+  return (
+    <div className="hidden lg:flex flex-col w-[206px] space-y-6 pl-5 pt-5 bg-red-500">
+      Org Sidebar
+    </div>
+  );
+}
