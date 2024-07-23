63af7d2f8534f19ba9bc2401d94ac9070f78b07c
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-07-23 16:35:05 +0530

    feat: create board using convex mutations

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,27 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { useOrganization } from "@clerk/nextjs";
 import Image from "next/image";
+import { toast } from "sonner";
 
 const EmptyBoards = () => {
+  const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) return;
+
+    mutate({
+      orgId: organization.id,
+      title: "Untitled Board",
+    })
+      .then((_id) => toast.success("Board created successfully!"))
+      .catch((_error) => toast.error("Failed to create board"));
+  };
+
   return (
     <div className="h-full flex flex-col justify-center items-center">
       <Image src="/note.svg" height={110} width={110} alt="Empty" />
@@ -10,7 +30,9 @@ const EmptyBoards = () => {
         Start by creating a new board or finding a board to collaborate on.
       </p>
       <div className="mt-6">
-        <Button size="lg">Create Board</Button>
+        <Button disabled={pending} onClick={onClick} size="lg">
+          Create Board
+        </Button>
       </div>
     </div>
   );
