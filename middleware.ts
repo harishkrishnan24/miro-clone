d095340a1429c2431c6031b7b16027286feeb099
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-07-23 15:49:30 +0530

    feat: empty organization view

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
     auth().protect();
   }
 });
