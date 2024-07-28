7e1d015550f963f71185a29bece0e2de5954d5e5
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-07-28 11:57:59 +0530

    feat:show particpant list

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,21 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+const COLORS = [
+  "#DC3545",
+  "#F8312F",
+  "#F9C23C",
+  "#17A2B8",
+  "#28A745",
+  "#343A40",
+  "#FFC107",
+  "#212529",
+];
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length];
 }
