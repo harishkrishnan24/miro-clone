2f4cfc4360b51945eb26af6836a87a69cd450aa6
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-07-28 11:41:09 +0530

    feat: info component in canvas page

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -14,10 +14,10 @@ const Toolbar = () => {
   );
 };
 
-Toolbar.Skeleton = function ToolbarSkeleton() {
+export function ToolbarSkeleton() {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 bg-white h-[360px] w-[52px] shadow-md rounded-md" />
   );
-};
+}
 
 export default Toolbar;
