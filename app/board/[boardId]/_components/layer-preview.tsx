84eac8e03c40f8054f5a80b76120c4daee657f81
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-07-31 13:56:05 +0530

    feat: text layer

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -5,6 +5,7 @@ import { useStorage } from "@liveblocks/react/suspense";
 import { memo } from "react";
 import Rectangle from "./rectangle";
 import Ellipse from "./ellipse";
+import Text from "./text";
 
 interface LayerPreviewProps {
   id: string;
@@ -33,6 +34,15 @@ const LayerPreview = memo(
       case LayerType.Ellipse:
         return (
           <Ellipse
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+      case LayerType.Text:
+        return (
+          <Text
             id={id}
             layer={layer}
             onPointerDown={onLayerPointerDown}
